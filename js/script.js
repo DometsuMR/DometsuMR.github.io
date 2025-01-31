471cae1411deb8607774efc308a124c5607dcae5
DometsuMR <dometsumr@gmail.com>
2025-01-31 16:59:16 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -34,17 +34,6 @@ fetch('js/json/data.json')
         document.getElementById("lista-proyectos").innerHTML = proyectosHTML;
     });
 
-document.getElementById("contact-form").addEventListener("submit", function(event) {
-    event.preventDefault();
-    
-    emailjs.sendForm("service_l8juotg", "template_e7rv45n", this, "EtE00FZeRLtEJmtsb")
-        .then(function(response) {
-            document.getElementById("respuesta").innerText = "¡Mensaje enviado con éxito!";
-        }, function(error) {
-            document.getElementById("respuesta").innerText = "Hubo un error al enviar el mensaje.";
-            console.error('Error al enviar el correo:', error);  // Detalles del error
-        });
-});
 
 
 
