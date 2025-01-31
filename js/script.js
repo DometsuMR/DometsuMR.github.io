421cb5a3a42274c279a6f59c7002ad04af49de44
DometsuMR <dometsumr@gmail.com>
2025-01-31 16:47:28 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -36,12 +36,15 @@ fetch('js/json/data.json')
 
 document.getElementById("contact-form").addEventListener("submit", function(event) {
     event.preventDefault();
+    
     emailjs.sendForm("service_l8juotg", "template_e7rv45n", this, "EtE00FZeRLtEJmtsb")
         .then(function(response) {
             document.getElementById("respuesta").innerText = "¡Mensaje enviado con éxito!";
         }, function(error) {
             document.getElementById("respuesta").innerText = "Hubo un error al enviar el mensaje.";
+            console.log("Error al enviar el correo: ", error);
         });
 });
+
 
 
